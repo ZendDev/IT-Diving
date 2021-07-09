9c13596fdc2761867e9d0b201285d939de092750
John Doe <zend.dev@yandex.ru>
2021-07-09 11:05:12 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,77 @@
-jQuery(window).scroll(function(){$("section").each(function(t,e){var n=$(e).offset().top-300,a=n+$(e).height(),i=$(window).scrollTop(),e=$(e).attr("id");n<i&&i<a&&($("a.active").removeClass("active"),$('a[href="#'+e+'"]').addClass("active"))})}),$(".it-mobile-menu").on("click",function(){$(".it-navbar").addClass("it-navbar_open")}),$(".it-mobile-close").on("click",function(){$(".it-navbar").removeClass("it-navbar_open")}),$(".it-navbar__link").on("click",function(){$(".it-navbar").removeClass("it-navbar_open")}),$(".it-button_white").on("click",function(){$(".modal").addClass("modal--open")}),$(".modal-close").on("click",function(){$(".modal").removeClass("modal--open")}),$(".it-navbar").on("click","a",function(t){t.preventDefault();t=$(this).attr("href"),t=$(t).offset().top-200;$("body,html").animate({scrollTop:t},800)});let buttons=$(".it-card__list"),button=buttons.find("span"),sections=$(".sections"),section=sections.find("div");function defaultState(){$(button[0]).trigger("click")}button.on("click",function(n){var a=$(this).data("content");button.removeClass("active"),$(this).addClass("active"),section.removeClass("-active"),section.each(function(t,e){a==$(e).data("section")&&($(e).addClass("-active"),animation(n,e))})});let arr=[];function animation(t,e){var n=$(t.target)[0];arr.push(e),2<arr.length&&arr.shift(),button.each(function(t,e){n===e&&($(arr[0]).data("section")>t&&($(arr).removeClass(),$(arr[0]).addClass("-rightOut"),$(arr[1]).addClass("-leftIn"),console.log("ESQUERDA",arr)),$(arr[0]).data("section")<t&&($(arr).removeClass(),$(arr[0]).addClass("-leftOut"),$(arr[1]).addClass("-rightIn"),console.log("DIREITA",arr)))})}defaultState();const elem=document.getElementById("it_map_1"),elem1=document.getElementById("it_map_2"),elem2=document.getElementById("it_map_3");document.addEventListener("scroll",function(){var t=elem.getBoundingClientRect().top+200,e=elem1.getBoundingClientRect().top-450,n=elem2.getBoundingClientRect().top;elem.classList.toggle("it-map__item_active",t+elem.clientHeight<=window.innerHeight&&450<=t),elem1.classList.toggle("it-map__item_active",e<=0),elem2.classList.toggle("it-map__item_active",n+elem2.clientHeight<=window.innerHeight&&0<=n)}),window.addEventListener("DOMContentLoaded",function(){var l;function t(t){t.keyCode&&(l=t.keyCode),this.selectionStart<3&&t.preventDefault();var e="+7 (___) ___-__-__",n=0,a=e.replace(/\D/g,""),i=this.value.replace(/\D/g,""),o=e.replace(/[_\d]/g,function(t){return n<i.length?i.charAt(n++)||a.charAt(n):t});-1!=(n=o.indexOf("_"))&&(n<5&&(n=3),o=o.slice(0,n));e=e.substr(0,this.value.length).replace(/_+/g,function(t){return"\\d{1,"+t.length+"}"}).replace(/[+()]/g,"\\$&");(!(e=new RegExp("^"+e+"$")).test(this.value)||this.value.length<5||47<l&&l<58)&&(this.value=o),"blur"==t.type&&this.value.length<5&&(this.value="")}var e=document.querySelector("#tel1");e.addEventListener("input",t,!1),e.addEventListener("focus",t,!1),e.addEventListener("blur",t,!1),e.addEventListener("keydown",t,!1)});
+jQuery(window).scroll(function () {
+    $("section").each(function (t, e) {
+        var n = $(e).offset().top - 300,
+            a = n + $(e).height(),
+            i = $(window).scrollTop(),
+            e = $(e).attr("id");
+        n < i && i < a && ($("a.active").removeClass("active"), $('a[href="#' + e + '"]').addClass("active"));
+    });
+}),
+    $(".it-mobile-menu").on("click", function () {
+        $(".it-navbar").addClass("it-navbar_open");
+    }),
+    $(".it-mobile-close").on("click", function () {
+        $(".it-navbar").removeClass("it-navbar_open");
+    }),
+    $(".it-navbar__link").on("click", function () {
+        $(".it-navbar").removeClass("it-navbar_open");
+    }),
+    $(".it-button_white").on("click", function () {
+        $(".modal").addClass("modal--open");
+    }),
+    $(".modal-close").on("click", function () {
+        $(".modal").removeClass("modal--open");
+    }),
+    $(".it-navbar").on("click", "a", function (t) {
+        t.preventDefault();
+        (t = $(this).attr("href")), (t = $(t).offset().top - 200);
+        $("body,html").animate({ scrollTop: t }, 800);
+    });
+    $('.sections').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false
+    });
+    function num(value){
+        $('.sections').slick('slickGoTo', value)
+    }
+      
+    $('.it-card__item').on('click', function(){
+        $('.it-card__item').removeClass('it-card__item_active');
+        $(this).addClass('it-card__item_active');
+    })
+const elem = document.getElementById("it_map_1"),
+    elem1 = document.getElementById("it_map_2"),
+    elem2 = document.getElementById("it_map_3");
+document.addEventListener("scroll", function () {
+    var t = elem.getBoundingClientRect().top + 200,
+        e = elem1.getBoundingClientRect().top - 450,
+        n = elem2.getBoundingClientRect().top;
+    elem.classList.toggle("it-map__item_active", t + elem.clientHeight <= window.innerHeight && 450 <= t),
+        elem1.classList.toggle("it-map__item_active", e <= 0),
+        elem2.classList.toggle("it-map__item_active", n + elem2.clientHeight <= window.innerHeight && 0 <= n);
+}),
+    window.addEventListener("DOMContentLoaded", function () {
+        var l;
+        function t(t) {
+            t.keyCode && (l = t.keyCode), this.selectionStart < 3 && t.preventDefault();
+            var e = "+7 (___) ___-__-__",
+                n = 0,
+                a = e.replace(/\D/g, ""),
+                i = this.value.replace(/\D/g, ""),
+                o = e.replace(/[_\d]/g, function (t) {
+                    return n < i.length ? i.charAt(n++) || a.charAt(n) : t;
+                });
+            -1 != (n = o.indexOf("_")) && (n < 5 && (n = 3), (o = o.slice(0, n)));
+            e = e
+                .substr(0, this.value.length)
+                .replace(/_+/g, function (t) {
+                    return "\\d{1," + t.length + "}";
+                })
+                .replace(/[+()]/g, "\\$&");
+            (!(e = new RegExp("^" + e + "$")).test(this.value) || this.value.length < 5 || (47 < l && l < 58)) && (this.value = o), "blur" == t.type && this.value.length < 5 && (this.value = "");
+        }
+        var e = document.querySelector("#tel1");
+        e.addEventListener("input", t, !1), e.addEventListener("focus", t, !1), e.addEventListener("blur", t, !1), e.addEventListener("keydown", t, !1);
+    });
